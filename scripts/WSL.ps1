cd086f639b70df8b467281ad851becdaa060734a
Joseph Petersen <jpe@specshell.com>
2021-08-11 14:23:03 +0200

    update wsl ubuntu to choco install

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,17 +1,9 @@
 choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 
-#--- Ubuntu ---
-# TODO: Move this to choco install once --root is included in that package
-$item = "wslubuntu2004"
-$file = "$env:TEMP\$item.appx"
-Write-Host "Downloading $item"
-curl.exe -sL https://aka.ms/$item -o $file
-Add-AppxPackage $file
-Remove-Item $file
-# run the distro once and have it install locally with root user, unset password
+if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
+  choco install wsl-ubuntu-2004 --params "/InstallRoot:true"
 
-RefreshEnv
-Ubuntu2004 install --root
-Ubuntu2004 run apt update
-Ubuntu2004 run apt upgrade -y
-
+  RefreshEnv
+  Ubuntu2004 run apt update
+  Ubuntu2004 run apt upgrade -y
+}
